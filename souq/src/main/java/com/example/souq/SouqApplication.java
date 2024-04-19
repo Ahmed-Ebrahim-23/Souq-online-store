f9233ffc01bfd876d5d5cd2f928b7441b1373a71
AhmedEbrahim2322004 <ahmed2322004ahmed@gmail.com>
2024-04-19 16:20:16 +0200

    intial commit

---
+++ souq/src/main/java/com/example/souq/SouqApplication.java
@@ -0,0 +1,13 @@
+package com.example.souq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouqApplication.class, args);
+	}
+
+}
---
+++ souq/src/test/java/com/example/souq/SouqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.souq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
